fc765f0be008b02f605acca122e8ff4bcb7ce891
alumno <alumno@A12103>
2016-04-28 14:04:30 +0200

    generaraleatorios

---
+++ ciclodeinstruccion/src/ciclodeinstruccion/GeneradorAleatorios.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ciclodeinstruccion;
+
+import java.util.Random;
+
+/**
+ *
+ * @author alumno
+ */
+public class GeneradorAleatorios {
+    public int generarAleatorio(int tope){
+        Random aleatorio= new Random();
+        return aleatorio.nextInt(tope);
+    }
+}
