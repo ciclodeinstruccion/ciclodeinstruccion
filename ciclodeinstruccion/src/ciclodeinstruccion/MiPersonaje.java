9d7aaeb8025878dcaa9eb277d38ff32c29935490
alumno <alumno@A12103>
2016-04-28 13:46:21 +0200

    asdasdasddfsd

--- ciclodeinstruccion/src/ciclodeinstruccion/MiPersonaje.java
+++ ciclodeinstruccion/src/ciclodeinstruccion/MiPersonaje.java
@@ -14,14 +14,40 @@ public abstract class MiPersonaje implements Personajeable{
     private float bonusDaño;
     private int nivel;
     private int experiencia;
+    private int puntosNivel;
+    private final int AUMENTA_VIDA=10;//cantidad de vida que se aumenta al subir un punto la vida
+    private final int AUMENTA_DAÑO=5;//cantidad de daño que se aumenta al subir un punto el daño
+    private final int EXPERIENCIA_NECESARIA=1000;
 
-    public MiPersonaje(float bonusVida, float bonusDaño, int nivel, int experiencia) {
+    public MiPersonaje(float bonusVida, float bonusDaño, int nivel, int experiencia, int puntosNivel) {
         this.bonusVida = bonusVida;
         this.bonusDaño = bonusDaño;
         this.nivel = nivel;
         this.experiencia = experiencia;
+        this.puntosNivel=puntosNivel;
     }
-
+    public void aumentarExperiencia(int sumaExperiencia){
+        experiencia+=sumaExperiencia;
+        if (experiencia>=EXPERIENCIA_NECESARIA*nivel){
+            this.subeNivel();
+            experiencia-=EXPERIENCIA_NECESARIA*nivel;
+        }
+    }
+    public void gastarPuntosNivel(){
+        this.puntosNivel--;
+    }
+    public void aumentarBonusVida(){
+        this.bonusVida+=AUMENTA_VIDA;
+        this.gastarPuntosNivel();
+    }
+    public void aumentarBonusDaño(){
+        this.bonusDaño+=AUMENTA_DAÑO;
+        this.gastarPuntosNivel();
+    }
+    public void subeNivel(){
+        nivel++;
+        puntosNivel++;
+    }
     public float getBonusVida() {
         return bonusVida;
     }
@@ -36,6 +62,10 @@ public abstract class MiPersonaje implements Personajeable{
 
     public int getExperiencia() {
         return experiencia;
+    }
+
+    public int getPuntosNivel() {
+        return puntosNivel;
     }
 
     public void setBonusVida(float bonusVida) {
@@ -53,5 +83,10 @@ public abstract class MiPersonaje implements Personajeable{
     public void setExperiencia(int experiencia) {
         this.experiencia = experiencia;
     }
+
+    public void setPuntosNivel(int puntosNivel) {
+        this.puntosNivel = puntosNivel;
+    }
+    
     
 }
