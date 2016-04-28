564e2c03cab33419f8d31a710b070a5739e3621e
alumno <alumno@A12104>
2016-04-28 13:10:18 +0200

    Luis 1

---
+++ ciclodeinstruccion/src/ciclodeinstruccion/Usuarios/Usuarios.java
@@ -0,0 +1,41 @@
+
+package ciclodeinstruccion.Usuarios;
+
+
+public class Usuarios {
+    
+    private String nombre;
+    private String correo;
+    private String contraseña;
+
+    public Usuarios(String nombre, String correo, String contraseña) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contraseña = contraseña;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+}
