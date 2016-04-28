96e09ddaeaa176d7b70581096be5ea765b92021d
alumno <alumno@A12104>
2016-04-28 13:09:23 +0200

    Administrador

---
+++ ciclodeinstruccion/src/ciclodeinstruccion/Usuarios/Administrador.java
@@ -0,0 +1,54 @@
+
+package ciclodeinstruccion.Usuarios;
+
+import ciclodeinstruccion.Personaje;
+import ciclodeinstruccion.Usuarios.Usuarios;
+
+
+public class Administrador extends Usuarios{
+
+    public Administrador(String nombre, String correo, String contraseña) {
+        super(nombre, correo, contraseña);
+    }
+
+    public void modificarUsuarioNombre(Registrados r, String nombreNuevo){
+        
+        r.setNombre(nombreNuevo);  
+    }
+    
+    public void modificarUsuarioContraseña(Registrados r, String contraseñaNueva){
+        
+        r.setContraseña(contraseñaNueva);
+    }
+    
+    public void modificarUsuarioCorreo(Registrados r, String CorreoNuevo){
+        
+        r.setCorreo(CorreoNuevo);
+    }
+
+    public void eliminarUsuarios(Registrados r){
+        
+    }
+    
+    public void modificarPersonaje(Personaje p){
+        
+    }
+    
+    public void verUsuarios(listaDeUsuarios){
+        
+    }
+    
+    public void verPersonajes(listaDePersonajes){
+        
+    }
+    
+    public void historialDePartidas(listaDePartidas){
+        
+    }
+    
+    public void verEstadisticasAvanzadas(listaDePartidas){
+        
+    }
+    
+    
+}
