e500fba75d83d05694704d1b72c592646e7780fc
alumno <alumno@A12104>
2016-05-17 14:09:02 +0200

    bases de datos luis

--- ciclodeinstruccion/src/BaseDeDatos/consultasBD.java
+++ ciclodeinstruccion/src/BaseDeDatos/consultasBD.java
@@ -5,6 +5,16 @@
  */
 package BaseDeDatos;
 
+import Excepciones.ErrorBorrarAdministrador;
+import Excepciones.ErrorCrearAdministrador;
+import Excepciones.ErrorCrearPersonaje;
+import Personaje.Asesino;
+import Personaje.Fighter;
+import Personaje.Personaje;
+import Personaje.Tanque;
+import ciclodeinstruccion.Usuarios.Administrador;
+import java.sql.SQLException;
+
 /**
  *
  * @author alumno
@@ -24,5 +34,50 @@ public class consultasBD {
         return instancia;
     }
     
+    public void añadirAdministradores(Administrador a) throws ErrorCrearAdministrador{
+      
+        
+      try{
+          ConexionBD.instancia().getStatement().execute(
+          "INSERT INTO Administradores VALUES ('"+a.getNombre()+"','"+a.getCorreo()+"','"+a.getContraseña()+"')");
+      } catch (SQLException e) {
+          throw new ErrorCrearAdministrador();
+      }  
+    }
 
+    /*public void añadirPersonajes(Personaje p) throws ErrorCrearPersonaje{
+        
+        float especial=0;
+        
+        if(p instanceof Tanque){
+            Tanque t=(Tanque) p;
+            especial=t.getArmadura();
+        } else if(p instanceof Asesino){
+            Asesino a=(Asesino) p;
+            especial=a.getEsquivar();
+        } else if(p instanceof Fighter){
+            Fighter f=(Fighter) p;
+            especial=f.getCritico();
+        }
+        
+        
+        try{
+            ConexionBD.instancia().getStatement().execute(
+            "INSERT INTO Personajes VALUES ('"+p.getNombre()+"',"+p.getVida()+","+p.getDaño()+","+p.getPrecio()+",'"+p.getTipo()+"',"+especial+")");
+        }
+        catch (SQLException e){
+            throw new ErrorCrearPersonaje();
+        }
+    }*/
+
+    public void borrarAdministrador(String nombre) throws ErrorBorrarAdministrador{
+        
+        try{
+            ConexionBD.instancia().getStatement().execute(
+            "DELETE FROM ADMINISTRADORES WHERE nombre='"+nombre+"'"
+            );
+        } catch(SQLException e){
+            throw new ErrorBorrarAdministrador();
+        } 
+    }    
 }
